51e4d67d90fb3f74c8e243e048a3c85675ff7fda
邵励治 <shaolizhi1234@126.com>
2020-10-16 23:47:18 +0800

    创建了 ProjectItem 和 TagItem 的视图层代码

---
+++ src/views/index/components/ProjectItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- 时间壁垒 -->
+  <div class="product-item-container">
+    <div class="logo-container">
+      <img
+        :src="project.attributes.logo.attributes.url"
+        loading="lazy"
+        width="80"
+        height="80"
+        alt=""
+      />
+    </div>
+    <h4 class="product-name">{{ project.attributes.name }}</h4>
+    <p class="product-description">{{ project.attributes.description }}</p>
+    <p class="product-type">{{ project.attributes.type }}</p>
+    <div class="tag-container">
+      <tag-item
+        v-for="tag in project.attributes.tagListOfProject"
+        :key="tag.id"
+        :tag="tag"
+      ></tag-item>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AV from "leancloud-storage";
+import TagItem from "./TagItem.vue";
+
+export default defineComponent({
+  props: {
+    project: AV.Object
+  },
+  components: {
+    TagItem
+  }
+});
+</script>
---
+++ src/views/index/components/TagItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="tag-item" :style="{ background: tag.attributes.backgroundColor }">
+    <div class="tag-name">{{ tag.attributes.name }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AV from "leancloud-storage";
+
+export default defineComponent({
+  props: {
+    tag: AV.Object
+  }
+});
+</script>
